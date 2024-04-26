feb353fde5fa655ccc7b1b02b490a0c9384065a9
Konoplastiy Andrew <konoplastiy@gmail.com>
2024-04-26 23:53:52 +0300

    Added PlatformsInfo service and implemented the logic

--- src/app/shared/services/platform.service.ts
+++ src/app/shared/services/platform.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 export interface PlatformInfo {
   namePlatform: string;
@@ -12,6 +14,11 @@ export interface PlatformInfo {
   providedIn: 'root'
 })
 export class PlatformService {
+  private apiUrl: string = 'http://localhost:8081/api/v1/';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public getAllPlatformsInfo(): Observable<any> {
+    return this.httpClient.get(`${this.apiUrl}platforms`);
+  }
 }
